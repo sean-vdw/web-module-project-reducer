017e1ab967a346b19189250e0712d43d3db677f7
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-17 14:57:17 -0800

    clearDisplay functioning correctly

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
-import { applyNumber, changeOperation } from '../actions';
+import { applyNumber, changeOperation, clearDisplay } from '../actions';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -16,6 +16,10 @@ function App() {
 
   const operationHandler = (operation) => {
     dispatch(changeOperation(operation));
+  }
+
+  const clearDisplayHandler = () => {
+    dispatch(clearDisplay());
   }
 
   return (
@@ -65,7 +69,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={() => {clearDisplayHandler()}} value={"CE"}/>
             </div>
 
           </form>

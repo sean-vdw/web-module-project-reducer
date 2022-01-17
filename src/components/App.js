f9002fc075ddf144a38df33e6e210a79d72e258b
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-01-17 14:22:38 -0800

    imported and updated App.js to use useReducer hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, { initialState } from '../reducers';
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
